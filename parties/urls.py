e19157770c1bf922fd53b6ea24468d4f3a562c52
iaramer <yaramer95@gmail.com>
2020-07-15 23:08:50 +0300

    Added endpoint 'parties/' and template for html pages

--- parties/urls.py
+++ parties/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^test$', views.test_response, name='test_response'),  # todo: temporary url, remove after a while
+    url(r'^$', views.index, name='index'),
 ]
--- parties/views.py
+++ parties/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
+# todo: temporary method, remove after a while
+def test_response(request):
     return HttpResponse("<h3>Starting Secret Santa party!</h3>")
+
+
+def index(request):
+    return render(request, 'parties/homePage.html')
