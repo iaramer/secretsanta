2403da1be1b1fbe79a2b094ff33702011940528a
Iaroslav Amerkhanov <yaramer95@gmail.com>
2020-08-19 20:52:26 +0300

    Added main urls

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     url(r'^home$', views.home, name='home'),
+    url(r'^welcome$', views.welcome, name='welcome'),
+    url(r'^signup$', views.signup, name='signup'),
+    url(r'^login$', views.login, name='login'),
+    url(r'^party$', views.party, name='party'),  # todo: add the following pattern '/party/{id}'
 ]
-
-# /welcome
-# /signup
-# /login
-# /home
-# /party/{id}
--- core/views.py
+++ core/views.py
@@ -3,3 +3,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'core/homePage.html')
+
+
+def welcome(request):
+    return render(request, 'core/homePage.html')
+
+
+def signup(request):
+    return render(request, 'core/homePage.html')
+
+
+def login(request):
+    return render(request, 'core/homePage.html')
+
+
+def party(request):
+    return render(request, 'core/homePage.html')
