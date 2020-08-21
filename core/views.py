d7c7fe6cce27b8ee6a66e12fde003247bec5d7a9
Iaroslav Amerkhanov <yaramer95@gmail.com>
2020-08-21 22:14:39 +0300

    Added signup form for showing it on page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from core.models import User
+
 
 def home(request):
     return render(request, 'core/homePage.html')
@@ -10,7 +12,19 @@ def welcome(request):
 
 
 def signup(request):
-    return render(request, 'core/signup.html')
+    users = User.objects.all()
+
+    if request.method == 'POST':
+        user = User()
+        user.first_name = request.POST.get('fname')
+        user.second_name = request.POST.get('sname')
+        user.email = request.POST.get('eml')
+        user.password = request.POST.get('pwd')
+        user.save()
+
+        request.method = 'GET'
+
+    return render(request, 'core/signup.html', {'users': users})
 
 
 def login(request):
